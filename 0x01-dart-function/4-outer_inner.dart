231f50b3527d030aa001068b7a7829e17f3b685b
CKCarr <119520583+CKCarr@users.noreply.github.com>
2024-06-09 23:38:50 -0500

    spell check

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -26,7 +26,7 @@
 void outer(String name, String id) {
   String inner() {
     List<String> names = name.split(" ");
-    return 'Hello agent ${names[1][0]}.${names[0]} your id is ${id}';
+    return 'Hello Agent ${names[1][0]}.${names[0]} your id is ${id}';
   }
 
   print(inner());
